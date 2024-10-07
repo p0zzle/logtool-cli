17ebdd5be6f1a98e2edc138a9c346cc3272dbdab
Leon Collazo Arechaga <47929977+p0zzle@users.noreply.github.com>
2024-10-07 14:28:45 -0300

    Minor Upload

---
+++ logtool-cli.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+version="1.0"
+# A command line tool meant to log a [/filepath/location] and save it to
+# a [folder/location] or a zip, tar.gz, or tar.
+
+echo "logtool-cli $version"
+
+# variables. Common.
+
+log_directory='/path/to'
+archival_directory='/path/to'
+
+# The script is meant to take the regular /var/log path, if not stated otherwise it will go to this path by default.
+
+tar -czvf logs.tar.gz /var/log
+
+# as tar creates the file as-is in the folder that you are working in, we move it to a set destination
+
+cp logs.tar.gz  $archival_directory
